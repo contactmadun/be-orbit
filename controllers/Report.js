40bb3bd4a62941b919d89add9a4e8bb1ff1418c0
Maulana <contact.madun@gmail.com>
2025-10-14 15:13:04 +0700

    add logic profit

--- controllers/Report.js
+++ controllers/Report.js
@@ -1,5 +1,5 @@
 const { Reports } = require('../models')
-const { Op } = require('sequelize')
+const { Op, fn, col, literal } = require('sequelize')
 
 exports.getData = async (req, res) => {
   try {
@@ -24,6 +24,40 @@ exports.getData = async (req, res) => {
     return res.json({
       totalProfit,
       data: reports
+    })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ error: "Internal server error" })
+  }
+}
+
+exports.getDataProfitLimit = async (req, res) => {
+   try {
+    const { storeId, startDate, endDate } = req.query
+
+    if (!storeId || !startDate || !endDate) {
+      return res.status(400).json({ error: "storeId, startDate, endDate wajib dikirim" })
+    }
+
+    const reports = await Reports.findAll({
+      where: {
+        storeId,
+        createdAt: {
+          [Op.between]: [new Date(startDate), new Date(endDate)]
+        }
+      },
+      attributes: ['id', 'createdAt', 'totalProfit'], // cukup ambil yang penting saja
+      order: [['createdAt', 'ASC']]
+    })
+
+    // Format agar mudah dipakai di FE
+    const formatted = reports.map(r => ({
+      date: r.createdAt.toISOString().split('T')[0], // contoh: "2025-10-12"
+      totalProfit: parseFloat(r.totalProfit || 0)
+    }))
+
+    return res.json({
+      data: formatted
     })
   } catch (err) {
     console.error(err)
--- routes/report.js
+++ routes/report.js
@@ -1,9 +1,10 @@
-const { getData } = require('../controllers/Report');
+const { getData, getDataProfitLimit } = require('../controllers/Report');
 const authMiddleware = require('../middleware/authMiddleware');
 
 var express = require('express');
 var router = express.Router();
 
 router.get('/', getData);
+router.get('/profit-limit', getDataProfitLimit);
 
 module.exports = router;
